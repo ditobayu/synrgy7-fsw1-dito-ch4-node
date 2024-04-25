5b85dab9b045fb87e3b8af71161b01130226d699
ditobayu <ditobayuzs1@gmail.com>
2024-04-25 20:53:25 +0700

    feat: add user module

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 const {volumeKubus, luasPermukaanKubus} = require('./kubus');
 const {volumeBalok, luasPermukaanBalok} = require('./balok');
 const {volumePrismaSegitiga, luasPermukaanPrismaSegitiga} = require('./prisma');
+const {writeUserData, readUserData} = require('./user');
 
-console.log("Kubus"); 
-console.log(volumeKubus(8)); // 512
-console.log(luasPermukaanKubus(8)); // 384 
+// console.log("Kubus"); 
+// console.log(volumeKubus(8)); // 512
+// console.log(luasPermukaanKubus(8)); // 384 
 
-console.log("Balok"); 
-console.log(volumeBalok(8, 3, 4)); // 96
-console.log(luasPermukaanBalok(8, 3, 4)); // 112
+// console.log("Balok"); 
+// console.log(volumeBalok(8, 3, 4)); // 96
+// console.log(luasPermukaanBalok(8, 3, 4)); // 112
 
-console.log("Prisma"); 
-console.log(volumePrismaSegitiga(8, 3, 4)); // 96
-console.log(luasPermukaanPrismaSegitiga(8, 3, 4)); // 112
+// console.log("Prisma"); 
+// console.log(volumePrismaSegitiga(8, 3, 4)); // 96
+// console.log(luasPermukaanPrismaSegitiga(8, 3, 4)); // 112
+
+// const volumeBalok1 = volumeBalok(8, 3, 4); // 96
+
+// fs.writeFileSync('./result.txt', volumeBalok1.toString(), 'utf-8');
+
+writeUserData()
